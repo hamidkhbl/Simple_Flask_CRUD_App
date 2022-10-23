28d216e003ae13eb006fb771b16f892a9ddddbab
Hamid Khodabandehloo <hamid@dropbox.com>
2022-10-23 00:53:01 -0700

    Add update items

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from flask import jsonify, redirect
+from crypt import methods
+from distutils.log import error
+from flask import jsonify, redirect, abort
 from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask import request
+import sys
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@localhost:5432/todoapp2'
@@ -22,15 +25,38 @@ def __repr__(self):
 
 @app.route('/')
 def index():
-    return render_template('index.html', data=Todo.query.all())
+    return render_template('index.html', data=Todo.query.order_by('id').all())
 
 @app.route('/todos/create', methods = ['POST'])
 def create_todo():
+    error = False
+    body = {}
     description = request.get_json()['description']
-    todo = Todo(description)
-    db.session.add(todo)
-    db.session.commit()
-    return jsonify({
-        'description': todo.desctiption
-    })
+    try:
+        todo = Todo(description)
+        db.session.add(todo)
+        db.session.commit()
+        body['description'] = todo.desctiption
+    except:
+        db.session.rollback()
+        error = True
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+    if error:
+        abort(400)
+    else:
+        return jsonify(body)
 
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def set_completed_todo(todo_id):
+    try:
+        completed = request.get_json()['completed']
+        todo = Todo.query.get(todo_id)
+        todo.completed = True
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return redirect(url_for('index'))
